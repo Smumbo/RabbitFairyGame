bb03c44b2ccc16816b89e341e003c8bfba736412
Jade <33168227+sakohju@users.noreply.github.com>
2023-02-03 13:04:47 -0700

    I'm tryin so hard on this jump man

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,16 +15,15 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float xMove = Input.GetAxisRaw("Horizontal"); // d key changes value to 1, a key changes value to -1
         float zMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
 
         rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed;
 
-        if (Input.GetAxis("Jump") > 0)
-        {
-            rb.AddForce(Vector2.up, ForceMode2D.Impulse); //this doesn't quite work perfectly yet
+        if(Input.GetButtonDown("Jump")){
+            rb.AddForce(Vector2.up, ForceMode2D.Impulse);
         }
     }
 }
