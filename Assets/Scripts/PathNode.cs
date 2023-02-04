f541df4b79d4322e4c121994f677ae3d208efe14
Jonah Venglarcik <jonahvenglarcik@gmail.com>
2023-02-04 12:59:41 -0700

    Added path nodes where paths must start

---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour
+{
+    private bool stopDrawing;
+
+    private void Start()
+    {
+        stopDrawing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If player is done drawing and we need to disable drawing, disable it
+        if (stopDrawing && !DrawPath.instance.IsDrawing())
+        {
+            SceneManager.instance.SetCanDraw(false);
+            stopDrawing = false;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        // Enable drawing when the cursor enters path node
+        SceneManager.instance.SetCanDraw(true);
+    }
+
+    private void OnMouseExit()
+    {
+        // Disable drawing when the cursor enters path node, iff player is not currently drawing
+        if (!DrawPath.instance.IsDrawing())
+        {
+            SceneManager.instance.SetCanDraw(false);
+        }
+        else
+        {
+            // Disable drawing once player stops drawing
+            stopDrawing = true;
+        }
+    }
+}
