648f69e46c2e44c05914697ab1d8799ade3123e6
Jessica Coan <jcoan188@gmail.com>
2023-02-03 20:22:11 -0700

    Good physics I hope

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public int speed;
+    public float speed;
     public float jumpForce;
+    public float weight;
+    public Vector2 groundCheckShift;
+    public float groundCheckDistance;
     private Rigidbody2D rb;
     public GameObject lastCheckpoint;
+    private GameObject lastCheckpoint;
 
     // Start is called before the first frame update
     void Start()
@@ -16,23 +20,48 @@ public class Player : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         float xMove = Input.GetAxisRaw("Horizontal"); // d key changes value to 1, a key changes value to -1
-        float zMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
 
-        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed;
-
-
-        if(Input.GetButtonDown("Jump")){
-                //WHY DOESN'T RB.ADDFORCE WORK LIKE IT SHOULD STUPID PIECE OF CODE
+        Vector2 newVel = new Vector2((xMove * speed) - rb.velocity.x, 0);
+        
+        if (IsGrounded())
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                newVel.y = jumpForce;
+            }
         }
+        else
+        {
+            newVel /= weight / Time.fixedDeltaTime;
+            newVel.y += Physics2D.gravity.y * Time.fixedDeltaTime;
+        }
+
+        
+        rb.velocity += newVel;
     }
 
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(rb.position + groundCheckShift, Vector2.down * groundCheckDistance, groundCheckDistance);
+    }
 
-    public void GoToCheckpoint(){
-
-         this.transform.position = lastCheckpoint.transform.position;   
+    public void GoToCheckpoint() 
+    {
+        this.transform.position = lastCheckpoint.transform.position;
+        rb.velocity = Vector2.zero;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Death>() != null)
+        {
+            this.transform.position = lastCheckpoint.transform.position;
+        }
+        else if (collision.gameObject.GetComponent<Checkpoint>() != null)
+        {
+            lastCheckpoint = collision.gameObject;
+        }
     }
 }
