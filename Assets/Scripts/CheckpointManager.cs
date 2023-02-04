8cd0acbdc06cc39a631d3578ea8ab149207bf732
Jade <33168227+sakohju@users.noreply.github.com>
2023-02-03 19:08:46 -0700

    fixed a unity stupid

--- Assets/Scripts/CheckpointManager.cs
+++ Assets/Scripts/CheckpointManager.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
+public class CheckpointpointManager : MonoBehaviour
 {
+
+    public GameObject[] checkpoints;
+
     // Start is called before the first frame update
     void Start()
     {
