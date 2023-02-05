3b86755ec50f482e5168e299d50e2e5b62c0add5
Jade <33168227+sakohju@users.noreply.github.com>
2023-02-04 20:51:26 -0700

    death finally works the way I want

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -101,19 +101,13 @@ public class Player : MonoBehaviour
 
     public void GoToCheckpoint() 
     {
-        this.transform.position = lastCheckpoint.transform.position;
+        StartCoroutine(Die());
+        //this.transform.position = lastCheckpoint.transform.position;
         rb.velocity = Vector2.zero;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<Death>() != null)
-        {
-            //StartCoroutine(Die());
-            this.transform.position = lastCheckpoint.transform.position;
-            //animator.SetBool("Dead", false);
-
-        }
-        else if (collision.gameObject.GetComponent<Checkpoint>() != null)
+        if (collision.gameObject.GetComponent<Checkpoint>() != null)
         {
             lastCheckpoint = collision.gameObject;
         }
@@ -149,21 +143,21 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       /* MushroomNode mushroom = collision.gameObject.GetComponent<MushroomNode>();
+        MushroomNode mushroom = collision.gameObject.GetComponent<MushroomNode>();
         if (mushroom != null && mushroom.createdObject.activeSelf)
         {
             Vector2 dir = Vector2.Perpendicular(collision.contacts[0].normal) * Vector2.Dot(rb.velocity, collision.contacts[0].normal);
             dir.y = Mathf.Max(Mathf.Abs(dir.y), 1);
             rb.velocity += dir * bounceForce;
-        }*/
+        }
        
     }
 
-
     private IEnumerator Die(){
-        animator.SetBool("Dead", true);
-        yield return new WaitForSeconds(1f);
-        animator.SetBool("Dead", false);
+        animator.SetTrigger("Death");
+        yield return new WaitForSeconds(0.8f);
         this.transform.position = lastCheckpoint.transform.position;
     }
+
+
 }
