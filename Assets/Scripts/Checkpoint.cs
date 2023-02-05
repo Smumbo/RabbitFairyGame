5feabb451ba93a9134a29dfb9504e107e23359c6
Jade <33168227+sakohju@users.noreply.github.com>
2023-02-05 11:19:02 -0700

    added checkpoint woosh

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -6,10 +6,14 @@ public class Checkpoint : MonoBehaviour
 {
 
     public Sprite on;
+    private AudioSource woosh;
+    private bool isOn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        woosh = this.GetComponent<AudioSource>();
+        isOn = false;
     }
 
     // Update is called once per frame
@@ -21,10 +25,12 @@ public class Checkpoint : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.gameObject.GetComponent<Player>();
-        if (player != null)
+        if (player != null && !isOn)
         {
             player.lastCheckpoint = this.gameObject;
+            woosh.Play();
             this.GetComponent<SpriteRenderer>().sprite = on;
+            isOn = true;
         }
     }
 
