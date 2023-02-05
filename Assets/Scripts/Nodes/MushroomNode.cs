98f90699766bcf73ec07de007150abee04dcacfe
Jade <33168227+sakohju@users.noreply.github.com>
2023-02-04 18:34:09 -0700

    mushroom go boing

--- Assets/Scripts/Nodes/MushroomNode.cs
+++ Assets/Scripts/Nodes/MushroomNode.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class MushroomNode : Node
 {
+    private SpriteRenderer nodeSprite;
+    private Animator animator;
+
     private void OnMouseDown()
     {
         SceneManager.instance.ActivateNode(this);
+        nodeSprite = this.GetComponent<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     public override void Activate()
     {
         createdObject.SetActive(true);
+        nodeSprite.enabled = false;
     }
 
     public override void Deactivate()
     {
         createdObject.SetActive(false);
+        nodeSprite.enabled = true;
     }
 }
