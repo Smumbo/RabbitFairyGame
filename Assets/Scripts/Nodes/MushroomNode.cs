a97b31937266cf1b7e98069c436b61808740dcbd
Jade <33168227+sakohju@users.noreply.github.com>
2023-02-04 18:40:23 -0700

    set up mushroom animator

--- Assets/Scripts/Nodes/MushroomNode.cs
+++ Assets/Scripts/Nodes/MushroomNode.cs
@@ -25,4 +25,11 @@ public class MushroomNode : Node
         createdObject.SetActive(false);
         nodeSprite.enabled = true;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Player>() != null){
+            animator.SetTrigger("bounce");
+        }
+    }
 }
